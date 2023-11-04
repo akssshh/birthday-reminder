e400b59a5857533d2558c1d903622295b7a756a1
Akash Gogoi <gogoiakash09@gmail.com>
2023-11-04 20:51:26 +0530

    fix the not found page bug

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,18 +1,28 @@
-import Link from 'next/link'
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../components/bday-reminder.png";
 
 const AuthLayout = ({ children }) => {
   return (
     <>
-        <nav className='flex justify-between ' >
-            <h1>Birthday Reminder</h1>
-            <div className='flex gap-4' >
-            <Link href='/signup' >Sign up</Link>
-            <Link href='/login' >Log in</Link>
-            </div>
-        </nav>
-        {children}
+      <nav className="flex justify-between ">
+        <Link href="/">
+          <Image
+            src={Logo}
+            alt="Bday Reminder logo"
+            width={180}
+            placeholder="blur"
+            quality={100}
+          />
+        </Link>
+        <div className="flex gap-4">
+          <Link href="/signup">Sign up</Link>
+          <Link href="/login">Log in</Link>
+        </div>
+      </nav>
+      {children}
     </>
-  )
-} 
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
