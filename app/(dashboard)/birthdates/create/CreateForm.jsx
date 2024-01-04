26ec080c2a7e2de28b50d3d752bb05e648094d47
Akash Gogoi <gogoiakash09@gmail.com>
2024-01-04 14:47:06 +0530

    getting birthdates from supabase

--- app/(dashboard)/birthdates/create/CreateForm.jsx
+++ app/(dashboard)/birthdates/create/CreateForm.jsx
@@ -20,7 +20,7 @@ export default function CreateForm() {
       user_email: 'mario@netninja.dev'
     };
 
-    const res = await fetch("http://localhost:3000/api/birthdays", {
+    const res = await fetch("http://localhost:3000/api/birthdates", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(newBirthday),
